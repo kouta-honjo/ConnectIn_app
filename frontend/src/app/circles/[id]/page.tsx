733168193f73756ef95c60f86adc3818a57f21e8
kouta-honjo <kouchaman712@gmail.com>
2025-07-04 22:35:54 +0900

    k

--- frontend/src/app/circles/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CircleDetail from '@/components/CircleDetail';
-
-export default function CircleDetailPage() {
-  return (
-    <div>
-      <CircleDetail />
-    </div>
-  );
-}
--- frontend/src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserProfile from '@/components/UserProfile';
-
-export default function ProfilePage() {
-  return (
-    <div>
-      <UserProfile />
-    </div>
-  );
-}
